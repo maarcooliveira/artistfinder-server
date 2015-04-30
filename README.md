17b55853c5a13f2d7ec31edf268004c1d997bd78
Marco Oliveira <oliveira.marco.andre@gmail.com>
2015-04-30 19:58:48 +0000

    few changes to project info

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-# Demo server code for MP3
-You should be able to do
-`npm install` and `nodemon server.js`
-
-You migth have to install nodemon on the vagrant machine using
-`npm install -g nodemon`
-
-
+# Artist Finder
+Final project for CS 498RK @ UIUC.
--- server.js
+++ server.js
@@ -18,6 +18,7 @@ var port = process.env.PORT || 4000;
 var allowCrossDomain = function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
   next();
 };
 app.use(allowCrossDomain);
